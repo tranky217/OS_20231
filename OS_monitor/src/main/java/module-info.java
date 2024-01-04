cd3887049b716195af1b44e51438a3ad0b0e1e5b
tranky217 <trantrongky217@gmail.com>
2024-01-04 07:00:13 +0700

    lastest

--- OS_monitor/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.os_monitor {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.os_monitor to javafx.fxml;
-    exports com.example.os_monitor;
-}
